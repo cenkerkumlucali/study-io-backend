4bf2aa8c08fd3c982158bf36e58be3d6b38eb320
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-18 18:07:10 +0300

    refactor

--- src/studyCloud/Application/Features/Feeds/Post/Queries/GetByIdPost/GetByIdPostQueryResponse.cs
+++ src/studyCloud/Application/Features/Feeds/Post/Queries/GetByIdPost/GetByIdPostQueryResponse.cs
@@ -5,7 +5,7 @@ namespace Application.Features.Feeds.Post.Queries.GetByIdPost;
 public class GetByIdPostQueryResponse
 {
     public int Id { get; set; }
-    public string FullName { get; set; }
+    public string FullName { get; set; } 
     public string Content { get; set; }
     public ICollection<PostCommentDto> Comments { get; set; }
     public int CommentCount { get; set; }
