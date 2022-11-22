66129dd3f6a699b1847cc202990268b718f99a2e
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-22 23:39:44 +0300

    created property for image or file Url

--- src/studyCloud/Domain/Entities/File.cs
+++ src/studyCloud/Domain/Entities/File.cs
@@ -8,6 +8,7 @@ public class File : BaseEntity
     public string FileName { get; set; }
     public string Path { get; set; }
     public string Storage { get; set; }
+    public string Url { get; set; }
     [NotMapped]
     public override DateTime UpdatedDate { get; set; }
 }
