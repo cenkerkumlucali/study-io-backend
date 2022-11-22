8827fd34f90dd16971697982f23c0b08983e9df0
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-22 23:40:40 +0300

    Refactor GetByIdPostQueryHandler.cs

--- src/studyCloud/Application/Features/Feeds/Post/Queries/GetByIdPost/GetByIdPostQueryHandler.cs
+++ src/studyCloud/Application/Features/Feeds/Post/Queries/GetByIdPost/GetByIdPostQueryHandler.cs
@@ -1,3 +1,5 @@
+using Application.Abstractions.Services;
+using Application.Abstractions.Storage.AWS;
 using Application.Repositories.Services.Feeds;
 using AutoMapper;
 using Domain.Entities.Comments;
@@ -8,13 +10,17 @@ namespace Application.Features.Feeds.Post.Queries.GetByIdPost;
 
 public class GetByIdPostQueryHandler : IRequestHandler<GetByIdPostQueryRequest, GetByIdPostQueryResponse>
 {
-    private IPostRepository _postRepository;
+    private readonly IPostRepository _postRepository;
+    private readonly ICommentImageFileService _commentImageFileService;
+    private readonly IAwsStorage _awsStorage;
     private IMapper _mapper;
 
-    public GetByIdPostQueryHandler(IPostRepository postRepository, IMapper mapper)
+    public GetByIdPostQueryHandler(IPostRepository postRepository, IMapper mapper, IAwsStorage awsStorage, ICommentImageFileService commentImageFileService)
     {
         _postRepository = postRepository;
         _mapper = mapper;
+        _awsStorage = awsStorage;
+        _commentImageFileService = commentImageFileService;
     }
 
     public async Task<GetByIdPostQueryResponse> Handle(GetByIdPostQueryRequest request,
@@ -23,12 +29,16 @@ public class GetByIdPostQueryHandler : IRequestHandler<GetByIdPostQueryRequest, 
         Domain.Entities.Feeds.Post? post =
             await _postRepository.GetAsync(c => c.Id == request.Id,
                 i => i.Include(u => u.User)
+                    .Include(c => c.PostImageFiles)
+                    .Include(c => c.Comments)
+                    .ThenInclude(c => c.CommentImageFiles)
                     .Include(c => c.Comments)
                     .ThenInclude(c => c.CommentLikes));
 
         if (post?.Comments is not null) post.Comments = post?.Comments.Where(c => c.ParentId == null).ToList();
-        GetByIdPostQueryResponse getByIdPostDto =
+        
+        GetByIdPostQueryResponse getByIdPostQueryResponse =
             _mapper.Map<GetByIdPostQueryResponse>(post);
-        return getByIdPostDto;
+        return getByIdPostQueryResponse;
     }
 }
