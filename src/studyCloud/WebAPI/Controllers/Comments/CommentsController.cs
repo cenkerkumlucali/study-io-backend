c1d1b00de5ba01e6e9e0cc2a399577f05afe9e8b
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-26 00:13:59 +0300

    Added Post Like operations

--- src/studyCloud/WebAPI/Controllers/Comments/CommentsController.cs
+++ src/studyCloud/WebAPI/Controllers/Comments/CommentsController.cs
@@ -3,17 +3,22 @@ using Application.Features.Comments.Comment.Commands.DeleteComment;
 using Application.Features.Comments.Comment.Commands.UpdateComment;
 using Application.Features.Comments.Comment.Models;
 using Application.Features.Comments.Comment.Queries.GetByIdComment;
+using Application.Features.Comments.Comment.Queries.GetByPostIdComment;
 using Application.Features.Comments.Comment.Queries.GetListComment;
 using Application.Features.Comments.CommentFile.Commands.CreateCommentFile;
 using Application.Features.Comments.CommentFile.Commands.DeleteCommentFile;
 using Application.Features.Comments.CommentFile.Queries.GetListCommentFile;
+using Application.Features.Comments.CommentLike.Commands.LikeComment;
+using Application.Features.Comments.CommentLike.Commands.UnLikeComment;
+using Application.Features.Comments.CommentLike.Queries.GetUsersLikedComment;
+using Application.Features.Feeds.PostLike.Queries.GetMembersLikedPost;
 using Microsoft.AspNetCore.Mvc;
 
 namespace WebAPI.Controllers.Comments;
 
 [Route("api/[controller]")]
 [ApiController]
-public class CommentsController:BaseController
+public class CommentsController : BaseController
 {
     [HttpPost]
     public async Task<IActionResult> Add(
@@ -54,6 +59,7 @@ public class CommentsController:BaseController
         GetByIdCommentQueryResponse result = await Mediator.Send(getByIdCommentQuery);
         return Ok(result);
     }
+
     [HttpPost("[action]")]
     public async Task<IActionResult> UploadImage(
         [FromQuery] CreateCommentImageFileCommandRequest createCommentFileCommandRequest)
@@ -78,5 +84,37 @@ public class CommentsController:BaseController
         deleteCommentFileCommandRequest.ImageId = imageId;
         DeleteCommentFileCommandResponse response = await Mediator.Send(deleteCommentFileCommandRequest);
         return Ok();
+    }
+
+    [HttpGet("[action]/{PostId}")]
+    public async Task<IActionResult> GetByPostId(
+        [FromRoute] GetByPostIdCommentQueryRequest getByPostIdCommentQueryRequest)
+    {
+        List<GetByPostIdCommentQueryResponse> response = await Mediator.Send(getByPostIdCommentQueryRequest);
+        return Ok(response);
+    }
+    
+    [HttpGet("[action]/{CommentId}")]
+    public async Task<IActionResult> GetMembersLikedComment(
+        [FromRoute] GetUsersLikedCommentQueryRequest getUsersLikedCommentQueryRequest)
+    {
+        List<GetUsersLikedCommentQueryResponse> response = await Mediator.Send(getUsersLikedCommentQueryRequest);
+        return Ok(response);
+    }
+
+    [HttpPost("[action]/{UserId}/{CommentId}")]
+    public async Task<IActionResult> Like(
+        [FromRoute] LikeCommentCommandRequest createCommentLikeCommand)
+    {
+        LikeCommentCommandResponse response = await Mediator.Send(createCommentLikeCommand);
+        return Ok(response);
+    }
+
+    [HttpDelete("[action]/{UserId}/{CommentId}")]
+    public async Task<IActionResult> UnLike(
+        [FromRoute] UnLikeCommentCommandRequest deleteCommentLikeCommand)
+    {
+        UnLikeCommentCommandResponse response = await Mediator.Send(deleteCommentLikeCommand);
+        return Ok(response);
     }
 }
