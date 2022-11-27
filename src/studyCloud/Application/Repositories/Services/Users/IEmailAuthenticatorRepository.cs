52c6117c136c304ffe5305aee1047d70ef303e34
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-27 22:57:23 +0300

    Created EmailAuthenticatorRepository

---
+++ src/studyCloud/Application/Repositories/Services/Users/IEmailAuthenticatorRepository.cs
@@ -0,0 +1,7 @@
+﻿using Domain.Entities.Users;
+
+namespace Application.Repositories.Services.Users;
+
+public interface IEmailAuthenticatorRepository : IAsyncRepository<EmailAuthenticator>, IRepository<EmailAuthenticator>
+{
+}
---
+++ src/studyCloud/Persistence/Repositories/Users/EmailAuthenticatorRepository.cs
@@ -0,0 +1,13 @@
+﻿using Application.Repositories.Services.Users;
+using Domain.Entities.Users;
+using Persistence.Contexts;
+
+namespace Persistence.Repositories;
+
+public class EmailAuthenticatorRepository : EfRepositoryBase<EmailAuthenticator, BaseDbContext>,
+                                            IEmailAuthenticatorRepository
+{
+    public EmailAuthenticatorRepository(BaseDbContext context) : base(context)
+    {
+    }
+}
