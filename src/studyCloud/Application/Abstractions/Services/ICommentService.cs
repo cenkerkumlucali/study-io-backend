d182e39084c69d0445aabc8abdb101c6ad76e1fd
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-30 19:52:39 +0300

    Added Upload and DeleteAllInPost

--- src/studyCloud/Application/Abstractions/Services/ICommentService.cs
+++ src/studyCloud/Application/Abstractions/Services/ICommentService.cs
@@ -1,8 +1,11 @@
 using Domain.Entities.Comments;
+using Domain.Entities.Feeds;
 
 namespace Application.Abstractions.Services;
 
 public interface ICommentService
 {
     Task<Comment?> GetById(int id);
+    Task<Comment> Upload(Comment comment);
+    Task DeleteAllInPost(Post post);
 }
--- src/studyCloud/Persistence/Services/CommentManager.cs
+++ src/studyCloud/Persistence/Services/CommentManager.cs
@@ -1,20 +1,50 @@
 using Application.Abstractions.Services;
 using Application.Repositories.Services.Comments;
 using Domain.Entities.Comments;
+using Domain.Entities.Feeds;
+using Domain.Entities.Users;
+using Domain.Enums;
+using Microsoft.EntityFrameworkCore;
 
 namespace Persistence.Services;
 
-public class CommentManager:ICommentService
+public class CommentManager : ICommentService
 {
     private ICommentRepository _commentRepository;
+    private readonly IMentionService _mentionService;
+    private readonly IAlarmService _alarmService;
+    private readonly IUserService _userService;
 
-    public CommentManager(ICommentRepository commentRepository)
+    public CommentManager(ICommentRepository commentRepository, IMentionService mentionService,
+        IAlarmService alarmService, IUserService userService)
     {
         _commentRepository = commentRepository;
+        _mentionService = mentionService;
+        _alarmService = alarmService;
+        _userService = userService;
     }
 
     public async Task<Comment?> GetById(int id)
     {
         return await _commentRepository.GetAsync(c => c.Id == id);
+    }
+
+    public async Task<Comment> Upload(Comment comment)
+    {
+        User user = await _userService.GetById(comment.UserId);
+        Comment? createdComment = await _commentRepository.AddAsync(comment);
+        Comment? getComment = await _commentRepository.GetAsync(c => c.Id == createdComment.Id,
+            include: c => c.Include(c => c.Post));
+
+        await _mentionService.MentionMembers(user, comment);
+        await _alarmService.Alert(AlarmType.COMMENT, comment.UserId, getComment.Post.UserId, getComment.PostId,
+            getComment.Id);
+        return getComment;
+    }
+
+    public async Task DeleteAllInPost(Post post)
+    {
+        IList<Comment> comments = (await _commentRepository.GetListAsync(c=>c.PostId == post.Id)).Items;
+        await _commentRepository.DeleteRangeAsync(comments);
     }
 }
