2cddc6ae8b2ff92d937ad1a0a8d89fad78f45e7e
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-11-30 19:57:00 +0300

    Fix mapping profile

--- src/studyCloud/Application/Features/Users/Block/Profiles/MappingProfiles.cs
+++ src/studyCloud/Application/Features/Users/Block/Profiles/MappingProfiles.cs
@@ -1,5 +1,7 @@
+using Application.Features.Quizzes.Answer.Queries.GetByIdAnswer;
 using Application.Features.Users.Block.Commands.CreateBlock;
 using Application.Features.Users.Block.Commands.DeleteBlock;
+using Application.Features.Users.Block.Queries.GetByUserId;
 using AutoMapper;
 
 namespace Application.Features.Users.Block.Profiles;
@@ -12,5 +14,11 @@ public class MappingProfiles : Profile
         CreateMap<Domain.Entities.Users.Block, CreateBlockCommandResponse>().ReverseMap();
         CreateMap<Domain.Entities.Users.Block, DeleteBlockCommandRequest>().ReverseMap();
         CreateMap<Domain.Entities.Users.Block, DeleteBlockCommandResponse>().ReverseMap();
+        CreateMap<Domain.Entities.Users.Block, GetByIdAnswerQueryRequest>().ReverseMap();
+        CreateMap<Domain.Entities.Users.Block, GetByUserIdQueryResponse>()
+            .ForMember(c => c.FullName, c => c.MapFrom(c => c.Target.FirstName + " " + c.Target.LastName))
+            .ForMember(c => c.UserName, c => c.MapFrom(c => c.Target.UserName))
+            .ForMember(c => c.PictureUrl, c => c.MapFrom(c => c.Target.UserImageFiles.First().Url))
+            .ReverseMap();
     }
 }
