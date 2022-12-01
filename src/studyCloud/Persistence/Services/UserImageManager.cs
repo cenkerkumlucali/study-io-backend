f481d629dd192f700fce48ff92fc4fc210b86a92
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-12-01 18:36:51 +0300

    Added ElasticSearch

--- src/studyCloud/Persistence/Services/UserImageManager.cs
+++ src/studyCloud/Persistence/Services/UserImageManager.cs
@@ -1,11 +1,15 @@
 using Application.Abstractions.Services;
+using Application.Abstractions.Services.ElasticSearch;
 using Application.Abstractions.Storage;
 using Application.Abstractions.Storage.AWS;
+using Application.DTOs.ElasticSearch;
 using Application.DTOs.Storage.AWS;
+using Application.Features.Users.User.Dtos;
 using Application.Repositories.Services.Users;
 using Domain.Entities.Users;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Nest;
 
 namespace Persistence.Services;
 
@@ -15,21 +19,23 @@ public class UserImageManager:IUserImageService
     private readonly IUserRepository _userRepository;
     private readonly IStorageService _storageService;
     private readonly IAwsStorage _awsStorage;
+    private readonly IElasticSearch _elasticSearch;
 
-    public UserImageManager(IStorageService storageService, IUserRepository userRepository, IUserImageRepository userImageRepository, IAwsStorage awsStorage)
+    public UserImageManager(IStorageService storageService, IUserRepository userRepository, IUserImageRepository userImageRepository, IAwsStorage awsStorage, IElasticSearch elasticSearch)
     {
         _storageService = storageService;
         _userRepository = userRepository;
         _userImageRepository = userImageRepository;
         _awsStorage = awsStorage;
+        _elasticSearch = elasticSearch;
     }
 
 
-    public async Task Upload(int commentId, IFormFileCollection files)
+    public async Task Upload(int userId, IFormFileCollection files)
     {
         List<(string fileName, string pathOrContainerName)> result =
             await _storageService.UploadAsync("study.io-user-image", files);
-        User? user = await _userRepository.GetAsync(c => c.Id == commentId);
+        User? user = await _userRepository.GetAsync(c => c.Id == userId);
         await _userImageRepository.AddRangeAsync(result.Select(r => new UserImageFile()
         {
             FileName = r.fileName,
@@ -38,6 +44,32 @@ public class UserImageManager:IUserImageService
             Url = $"http://study.io-user-image.s3.amazonaws.com/{r.pathOrContainerName}",
             Users = new List<User>() {user }
         }).ToList());
+        foreach (var value in result)
+        {
+            List<ElasticSearchGetModel<UserDto>> getUser = await
+                _elasticSearch.GetSearchByField<UserDto>(
+                    new SearchByFieldParameters
+                    {
+                        IndexName = "users",
+                        FieldName = "UserName",
+                        Value = user.UserName
+                    });
+
+            ElasticSearchInsertUpdateModel model = new()
+            {
+                ElasticId = getUser.FirstOrDefault()?.ElasticId,
+                IndexName = "users",
+                Item = new UserDto()
+                {
+                    Id = user.Id,
+                    FullName = user.FirstName + " " + user.LastName,
+                    UserName = user.UserName,
+                    PictureUrl = $"http://study.io-user-image.s3.amazonaws.com/{value.pathOrContainerName}"
+                }
+            };
+            await _elasticSearch.UpdateByElasticIdAsync(model);
+        }
+        
     }
 
     public async Task<List<UserImageFile>> GetAll()
