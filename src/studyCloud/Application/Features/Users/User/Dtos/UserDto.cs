80532a3cefe82f1a74d8d588173e887aca99ed67
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-12-02 15:21:27 +0300

    Created User Dto

---
+++ src/studyCloud/Application/Features/Users/User/Dtos/UserDto.cs
@@ -0,0 +1,9 @@
+namespace Application.Features.Users.User.Dtos;
+
+public class UserDto
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+    public string FullName { get; set; }
+    public string PictureUrl { get; set; }
+}
