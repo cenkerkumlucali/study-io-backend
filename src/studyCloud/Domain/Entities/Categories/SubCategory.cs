8255c2cb5802cc56a8c6a620e9de5f96e43e9a9b
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-12-05 18:43:31 +0300

    Added Parent SubCategory

--- src/studyCloud/Domain/Entities/Categories/SubCategory.cs
+++ src/studyCloud/Domain/Entities/Categories/SubCategory.cs
@@ -2,10 +2,11 @@ using BaseEntity = Domain.Entities.Common.BaseEntity;
 
 namespace Domain.Entities.Categories;
 
-public class SubCategory:BaseEntity
+public class SubCategory : BaseEntity
 {
     public string Name { get; set; }
     public int CategoryId { get; set; }
+    public int? ParentId { get; set; }
+    public virtual SubCategory Parent { get; set; }
     public virtual Category Category { get; set; }
-    
 }
