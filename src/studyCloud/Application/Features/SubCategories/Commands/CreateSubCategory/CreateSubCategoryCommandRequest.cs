e8b11064d737c4f406f1bd7d1582997de2a48cb9
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-12-07 00:49:09 +0300

    Added ParentId

--- src/studyCloud/Application/Features/SubCategories/Commands/CreateSubCategory/CreateSubCategoryCommandRequest.cs
+++ src/studyCloud/Application/Features/SubCategories/Commands/CreateSubCategory/CreateSubCategoryCommandRequest.cs
@@ -5,7 +5,6 @@ namespace Application.Features.SubCategories.Commands.CreateSubCategory;
 public class CreateSubCategoryCommandRequest:IRequest<CreateSubCategoryCommandResponse>
 {
     public int CategoryId { get; set; }
+    public int? ParentId { get; set; }
     public string Name { get; set; }
-    
-    
 }
