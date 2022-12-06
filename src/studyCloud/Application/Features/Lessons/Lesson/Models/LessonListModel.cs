f5f78d8a2b77a5e929c8ed1d4e2b79f7f6f8270d
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-12-07 00:49:40 +0300

    Deleted LessonListModel.cs

--- src/studyCloud/Application/Features/Lessons/Lesson/Models/LessonListModel.cs
+++
@@ -1,8 +0,0 @@
-using Application.Features.Lessons.Lesson.Queries.GetListLesson;
-
-namespace Application.Features.Lessons.Lesson.Models;
-
-public class LessonListModel
-{
-    public IList<GetListLessonQueryResponse> Items { get; set; }
-}
