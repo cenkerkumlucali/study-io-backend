91cdf655e0e38a8baee2c3b92be4022928d63a8a
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-12-07 00:51:44 +0300

    Added Parent

--- src/studyCloud/Domain/Entities/Lessons/LessonSubject.cs
+++ src/studyCloud/Domain/Entities/Lessons/LessonSubject.cs
@@ -5,8 +5,9 @@ namespace Domain.Entities.Lessons;
 public class LessonSubject : BaseEntity
 {
     public string Name { get; set; }
-    public int LessonId { get; set; }
     public int? ParentId { get; set; }
-    public virtual LessonSubject Parent { get; set; }
+    public int LessonId { get; set; }
     public virtual Lesson Lesson { get; set; }
+    public virtual LessonSubject Parent { get; set; }
+    public virtual ICollection<LessonSubject>? Children { get; set; }
 }
