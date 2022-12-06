b623adb519004c978e881858ec4ae8698d1114e1
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-12-07 00:49:26 +0300

    Added ElasticSearch

--- src/studyCloud/Application/Features/Users/User/Commands/DeleteUser/DeleteUserCommandHandler.cs
+++ src/studyCloud/Application/Features/Users/User/Commands/DeleteUser/DeleteUserCommandHandler.cs
@@ -1,3 +1,5 @@
+using Application.Abstractions.Services.ElasticSearch;
+using Application.DTOs.ElasticSearch;
 using Application.Features.Users.User.Dtos;
 using Application.Features.Users.User.Rules;
 using Application.Repositories.Services.Users;
@@ -9,15 +11,17 @@ namespace Application.Features.Users.User.Commands.DeleteUser;
 public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommandRequest, DeleteUserCommandResponse>
 {
     private readonly IUserRepository _userRepository;
+    private readonly IElasticSearch _elasticSearch;
     private readonly IMapper _mapper;
     private readonly UserBusinessRules _userBusinessRules;
 
     public DeleteUserCommandHandler(IUserRepository userRepository, IMapper mapper,
-        UserBusinessRules userBusinessRules)
+        UserBusinessRules userBusinessRules, IElasticSearch elasticSearch)
     {
         _userRepository = userRepository;
         _mapper = mapper;
         _userBusinessRules = userBusinessRules;
+        _elasticSearch = elasticSearch;
     }
 
     public async Task<DeleteUserCommandResponse> Handle(DeleteUserCommandRequest request, CancellationToken cancellationToken)
@@ -26,6 +30,20 @@ public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommandRequest
 
         Domain.Entities.Users.User mappedUser = _mapper.Map<Domain.Entities.Users.User>(request);
         Domain.Entities.Users.User deletedUser = await _userRepository.DeleteAsync(mappedUser);
+        List<ElasticSearchGetModel<UserDto>> getUser = await
+            _elasticSearch.GetSearchByField<UserDto>(
+                new SearchByFieldParameters
+                {
+                    IndexName = "users",
+                    FieldName = "UserName",
+                    Value = deletedUser.UserName
+                });
+        ElasticSearchModel model = new()
+        {
+            ElasticId = getUser.FirstOrDefault()?.ElasticId,
+            IndexName = "users",
+        };
+        await _elasticSearch.DeleteByElasticIdAsync(model);
         DeleteUserCommandResponse deletedUserDto = _mapper.Map<DeleteUserCommandResponse>(deletedUser);
         return deletedUserDto;
     }
