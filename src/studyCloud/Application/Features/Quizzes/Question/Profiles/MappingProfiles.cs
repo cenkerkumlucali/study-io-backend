6b901a7b68d4c0b6f55a49e0a0bdf4ef08beb15c
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-12-09 00:14:59 +0300

    Added GetListByQuizId

--- src/studyCloud/Application/Features/Quizzes/Question/Profiles/MappingProfiles.cs
+++ src/studyCloud/Application/Features/Quizzes/Question/Profiles/MappingProfiles.cs
@@ -4,6 +4,7 @@ using Application.Features.Quizzes.Question.Commands.DeleteQuestion;
 using Application.Features.Quizzes.Question.Commands.UpdateQuestion;
 using Application.Features.Quizzes.Question.Models;
 using Application.Features.Quizzes.Question.Queries.GetByIdQuestion;
+using Application.Features.Quizzes.Question.Queries.GetListByQuizId;
 using Application.Features.Quizzes.Question.Queries.GetListQuestion;
 using AutoMapper;
 
@@ -19,6 +20,11 @@ public class MappingProfiles:Profile
         CreateMap<Domain.Entities.Quizzes.Question, DeleteQuestionCommandRequest>().ReverseMap();
         CreateMap<Domain.Entities.Quizzes.Question, UpdateQuestionCommandResponse>().ReverseMap();
         CreateMap<Domain.Entities.Quizzes.Question, UpdateQuestionCommandRequest>().ReverseMap();
+        
+        CreateMap<IPaginate<Domain.Entities.Quizzes.Question>,GetByQuizIdModel>().ReverseMap();
+        CreateMap<Domain.Entities.Quizzes.Question, GetListByQuizIdQueryResponse>()
+            .ForMember(c=>c.Answers,opt=>opt.MapFrom(c=>c.Answers))
+            .ForMember(c=>c.ImageUrl,opt=>opt.MapFrom(c=>c.QuestionImages.FirstOrDefault().Url)).ReverseMap();
         
         CreateMap<IPaginate<Domain.Entities.Quizzes.Question>,QuestionListModel>().ReverseMap();
         CreateMap<Domain.Entities.Quizzes.Question,ListQuestionQueryResponse>().ReverseMap();
