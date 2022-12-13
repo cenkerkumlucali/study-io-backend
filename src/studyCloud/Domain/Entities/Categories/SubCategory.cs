169092909312b6588736fabf992d32722a478bf0
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-12-13 17:27:01 +0300

    Added Lessons and Children

--- src/studyCloud/Domain/Entities/Categories/SubCategory.cs
+++ src/studyCloud/Domain/Entities/Categories/SubCategory.cs
@@ -1,3 +1,4 @@
+using Domain.Entities.Lessons;
 using BaseEntity = Domain.Entities.Common.BaseEntity;
 
 namespace Domain.Entities.Categories;
@@ -9,4 +10,6 @@ public class SubCategory : BaseEntity
     public long? ParentId { get; set; }
     public virtual SubCategory Parent { get; set; }
     public virtual Category Category { get; set; }
+    public virtual List<Lesson> Lessons { get; set; }
+    public virtual List<SubCategory> Children { get; set; }
 }
