a1bc64ecfe775ef3782f3aa5698a419599f443b2
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2022-12-13 17:28:21 +0300

    added Category and CategoryId

--- src/studyCloud/Domain/Entities/Lessons/Lesson.cs
+++ src/studyCloud/Domain/Entities/Lessons/Lesson.cs
@@ -7,8 +7,10 @@ namespace Domain.Entities.Lessons;
 public class Lesson : BaseEntity
 {
     public string Name { get; set; }
-    public long SubCategoryId { get; set; }
+    public long? SubCategoryId { get; set; }
+    public long? CategoryId { get; set; }
     public virtual SubCategory SubCategory { get; set; }
+    public virtual Category Category { get; set; }
     public virtual ICollection<LessonSubject> LessonSubjects { get; set; }
     public virtual ICollection<Quiz> Quizzes { get; set; }
 }
